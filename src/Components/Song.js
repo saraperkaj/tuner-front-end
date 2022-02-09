e969c22f6bce998160d6d82149b9f6722b14f1d2
Taylor W <taylorwilliams@pursuit.org>
2022-02-08 19:43:12 -0500

    Added time

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,4 +1,8 @@
 function Song({ song }) {
-  return <div>Name {song.name}</div>;
+  return (
+    <div>
+      {song.name} {song.artist} {song.album} {song.time}
+    </div>
+  );
 }
 export default Song;
