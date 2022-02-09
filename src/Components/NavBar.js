36b20a423fb273e26d8509ef96ba5dc546bb5bcb
Taylor W <taylorwilliams@pursuit.org>
2022-02-08 22:18:04 -0500

    Added play buttons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <nav className='nav'>
-    <Link className='home-link' to="/">Home</Link> 
-    <Link className='songs-link' to="/songs">Songs</Link>
-  </nav>
+  return (
+    <nav className="nav">
+      <Link className="home-link" to="/">
+        Home
+      </Link>
+
+      <Link className="songs-link" to="/songs">
+        Songs
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,6 +1,12 @@
+import play from "./play.png";
+
 function Song({ song }) {
   return (
     <tr className="song-list">
+      <td>
+        {" "}
+        <img src={play} width="50" alt={play} />
+      </td>
       <td className="song">{song.name}</td>
       <td className="song">{song.artist}</td>
       <td className="song">{song.album}</td>
